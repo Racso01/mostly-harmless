888eecc3858f2d83c582c0e2c05c18e1104cc2b2
Filippo Valsorda <hi@filippo.io>
2018-07-05 22:12:34 -0400

    fleet: add SLOG and L2ARC to frood tank

--- fleet/frood.home.arpa/mkimg.frood.sh
+++ fleet/frood.home.arpa/mkimg.frood.sh
@@ -3,7 +3,8 @@ profile_frood() {
         kernel_cmdline="console=tty0 console=ttyACM0,115200"
         kernel_flavors="vanilla"
         kernel_addons="zfs spl"
-        apks="$apks zfs"
+        apks="$apks joe strace tmux htop wget ca-certificates rsync tcpdump tree pv"
+        apks="$apks zfs e2fsprogs gptfdisk ntfs-3g hdparm"
         apks="$apks linux-vanilla zfs-vanilla"
         apks="$apks linux-firmware"
 }
--- fleet/frood.home.arpa/run.sh
+++ fleet/frood.home.arpa/run.sh
@@ -5,8 +5,9 @@ cd aports
 git pull
 apk -U upgrade
 sudo -u build scripts/mkimage.sh \
-    --tag 3.8f0 \
+    --tag 3.8f1 \
 	--outdir /iso \
 	--arch x86_64 \
 	--repository http://dl-cdn.alpinelinux.org/alpine/v3.8/main \
+	--repository http://dl-cdn.alpinelinux.org/alpine/v3.8/community \
 	--profile frood
